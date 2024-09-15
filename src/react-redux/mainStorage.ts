cb0bd6faa992f8e3c3e80be3a8749de5c675d36a
Egor Ivanov <rust_lang_lover@proton.me>
2024-09-15 14:11:28 +0300

    // implemented redux for current_display_image

--- src/react-redux/mainStorage.ts
+++ src/react-redux/mainStorage.ts
@@ -1,12 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {screen_type} from "./bases/screen_type/screen_type";
 import {image_storage} from "./bases/image_storage/image_storage";
+import {current_display_image} from "./bases/current_display_image/current_display_image";
 
 
 const application = configureStore({
     reducer : {
         screen_type : screen_type.reducer,
-        image_storage : image_storage.reducer
+        image_storage : image_storage.reducer,
+        current_display_image : current_display_image.reducer
     }
 })
 
